1c1da9fb2a081954a531a128c26d63f7a972ad3a
Yakov Lemenkov <yakoff4@gmail.com>
2023-03-17 17:57:19 +0300

    add component

--- src/App.vue
+++ src/App.vue
@@ -16,67 +16,9 @@
           </el-row>
         </el-aside>
         <el-container>
-          <el-main
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-            provident recusandae iste repellat reprehenderit, ea reiciendis
-            tenetur! Rem fuga tempore similique magnam, voluptates facere, iure
-            adipisci, culpa aliquid deserunt est eos nisi iste quis quo dolor
-            incidunt officia consectetur eius. Deleniti magnam autem reiciendis
-            consequuntur itaque atque dicta beatae fugit obcaecati rem aliquid
-            provident dolor, facere doloremque vitae. Minima, expedita corrupti,
-            quibusdam officia atque accusamus quod nam autem officiis, soluta
-            qui sunt doloribus dolore voluptate cupiditate ab? Quod mollitia
-            consequatur incidunt recusandae cum nesciunt ducimus fugit aliquam
-            ipsum nostrum quis, officiis ipsam soluta sequi eveniet ab! A ut,
-            quasi aperiam delectus itaque reiciendis libero consectetur ea ad
-            culpa rem? Dolorum expedita sit quod pariatur maxime, libero qui!
-            Provident commodi deserunt porro temporibus dolorem est! Enim
-            blanditiis unde quis molestias voluptatibus ipsum animi quibusdam
-            aliquam. Omnis adipisci placeat ratione blanditiis quasi dolorem
-            praesentium ex. Architecto possimus nisi, molestiae facere quos
-            quisquam. Error repudiandae nulla repellendus, cum ad neque. Minima
-            assumenda officia, nihil labore earum, optio accusamus corporis sit,
-            saepe dolores doloremque ab error mollitia harum beatae velit ea
-            similique. Natus officia ad voluptatem qui asperiores ab corporis,
-            optio mollitia laboriosam, impedit quisquam velit placeat autem
-            fuga? Quod consequuntur doloribus rerum iste atque, harum, cumque
-            dolorem nam et magnam debitis quis quam aperiam distinctio
-            perferendis est sint id libero unde! Odit obcaecati labore, eum
-            voluptatum illo ipsam tempora architecto, optio dolorem
-            necessitatibus animi est corrupti. Soluta impedit aliquam nihil
-            dolores corporis eos officia dicta quod porro natus. Eius harum
-            corrupti doloremque aperiam id ipsa pariatur similique accusantium
-            cumque voluptates natus, obcaecati, hic veniam eligendi omnis
-            tempora. Quaerat, aliquam? Eaque incidunt in necessitatibus iste,
-            earum unde cum id architecto hic placeat, saepe qui. Sequi suscipit,
-            possimus, optio quae officiis tempora quibusdam ratione officia
-            voluptatibus sint deserunt voluptas obcaecati, commodi recusandae
-            ducimus nesciunt nisi. Deserunt reprehenderit, iste tenetur animi,
-            eveniet dignissimos non modi obcaecati soluta recusandae laborum
-            quis perferendis fugit nihil consequatur reiciendis rem saepe
-            corrupti sapiente. Officia unde inventore ut, magni, perferendis
-            maxime cumque tenetur delectus animi dignissimos reiciendis minima?
-            Quasi ipsum vero quis, odio dolores optio repudiandae. Dolor
-            eligendi itaque nemo laudantium reprehenderit libero, nisi similique
-            natus. Quo cumque, amet cupiditate nesciunt non dolor earum officia
-            consequuntur soluta recusandae id, praesentium minima nulla unde,
-            incidunt saepe hic sequi nisi impedit perspiciatis dignissimos?
-            Voluptatem dolor ut dignissimos error fugiat, iste velit reiciendis
-            iusto repudiandae quod exercitationem perspiciatis suscipit rem
-            officia inventore minima. Recusandae, laborum laboriosam laudantium
-            voluptates autem magni aspernatur, consequuntur totam commodi iure
-            veritatis ratione, molestias esse dolorum est? Est sapiente quaerat
-            repellendus, earum voluptates nam velit quis deserunt optio possimus
-            voluptas ducimus corrupti ad expedita. Praesentium doloremque
-            officia illo corporis minima esse a libero nemo sit, beatae debitis,
-            natus cupiditate doloribus voluptas voluptate modi odit quod. Fuga
-            magni, laboriosam velit incidunt deserunt eaque tempora in excepturi
-            fugiat, praesentium non aut illum. Quidem dignissimos ex perferendis
-            quia corrupti. Consequatur pariatur repudiandae nesciunt ipsum eos
-            aspernatur assumenda enim tempore. Debitis, amet praesentium.
-            Possimus molestias voluptatem, nam consectetur accusamus incidunt
-            ipsum nostrum quas atque?</el-main
-          >
+          <el-main> 
+            <FormComp></FormComp>
+          </el-main>
           <el-footer>Footer</el-footer>
         </el-container>
       </el-container>
@@ -84,8 +26,9 @@
   </div>
 </template>
 
-<script lang="ts">
- export default {}
+<script lang="ts" setup>
+import { defineAsyncComponent } from 'vue'
+const FormComp = defineAsyncComponent(() => import('./components/Form-auth.vue'))
 </script>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.use(ElementPlus)
 app.mount('#app')
